6c0e9be1feb5f62f009b3b35ba84765df0a3afb4
Nikhil <nikhilupadhyay1604@gmail.com>
2024-01-30 07:17:18 +0530

    Implemented JSX,Functional component

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement(
-    "h1",
-    { id: "heading" },
-    "Hello World from Nikhil!"
-  );
+
+
+//JSX -- HTML like syntax
+
+const jsxHeading = <h1>JSX Heading</h1>
+
+
+// React Component  --> i.Class based component ii.Functional component.
+
+const HeadingComponent = () =>{
+    return <h1>This is a heading Component.</h1>
+}
+
+// Component Composition
+
+const HeaderOne = () => (
+    <h1>This is HeaderOne.</h1>
+)
+
+// So this HeaderTwo component which is using HeaderOne component inside it ,so this is called component composition.
+const HeaderTwo = () => (
+    <>
+        <HeaderOne/>
+        <h1>This is HeaderTwo.</h1>
+    </>
+)
 const root = ReactDOM.createRoot(document.getElementById("root"));
   
-root.render(heading);
+root.render(<HeaderTwo/>);
