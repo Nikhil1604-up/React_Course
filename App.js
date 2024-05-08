3a70643e0b96c96f17544e555549ed1e1f11c744
Nikhil <nikhilupadhyay1604@gmail.com>
2024-05-08 07:21:10 +0530

    feat:implemneted restaurant card using json data and passing data through props

--- App.js
+++ App.js
@@ -41,29 +41,46 @@ const Header = () => {
   );
 };
 
-const RestaurantCard = () =>{
+const RestaurantCard = (props) =>{
+  const {resObj} = props;
+  console.log(resObj)
   return (
     <div className="res-card">
       <img className="res-logo" alt="BBQ" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ru7j7x8qqtvuzfuw7cdh" />
-      <h3>BBQ</h3>
-      <h4>Biryani,North Indian</h4>
-      <h4>4.5 stars</h4>
-      <h4>38 mins</h4>
+      <h3>{resObj.data.info.name}</h3>
+      {resObj.data.info.cuisines.map((el) => <h4>{el}</h4>)}
+      <h4>{resObj.data.info.avgRaiting} stars</h4>
+      <h4>{resObj.data.info.delTime} mins</h4>
   </div>
   )
   
-}     
+}   
+
+const resObj = {
+  data: {
+      "info": {
+        "id": "626148",
+        "name": "La Pino'z Pizza",
+        "cloudinaryImageId": "ezjogwtzx40ko0wlat5l",
+        "locality": "Salt Lake",
+        "areaName": "Karol Bagh",
+        "costForTwo": "₹400 for two",
+        "cuisines": [
+          "Pizzas",
+        ],
+        "avgRaiting":"4.2",
+        "delTime":"34"
+      }
+    }
+}
 
 const Body = () =>{
   return (
     <div className="body">
       <div className="search">Search</div>
       <div className="res-container">
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
+        <RestaurantCard resObj={resObj}/>
+        {/* <RestaurantCard resName="KFC" cuisine="Fast Food,Wings,Burgers"/> */}
       </div>
     </div>
   )
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # React Course
+
+   # Props --> Props stands for properties basically just like in function we pass arguments similarly props are arguments which we pass into the component.
+
+   # Config Driven UI --> Lets suppose the case of swiggy website they show offers and coupons based on your locations and every locations will have some differenet offers so the developer will not make different set of offers for each location ,they will be controlling the UI using data or using configs which will be different for different locations as provided by backend API's. 
