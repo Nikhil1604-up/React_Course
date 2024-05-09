5ddf919d479b6c1d337f748481603fee4a9fd86a
Nikhil <nikhilupadhyay1604@gmail.com>
2024-05-09 06:53:51 +0530

    feat:added restaurant card using live data using map function

--- App.js
+++ App.js
@@ -43,21 +43,23 @@ const Header = () => {
 
 const RestaurantCard = (props) =>{
   const {resObj} = props;
-  console.log(resObj)
+  const {imageData,name,cuisines,avgRaiting,delTime} = resObj.info
   return (
-    <div className="res-card">
-      <img className="res-logo" alt="BBQ" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ru7j7x8qqtvuzfuw7cdh" />
-      <h3>{resObj.data.info.name}</h3>
-      {resObj.data.info.cuisines.map((el) => <h4>{el}</h4>)}
-      <h4>{resObj.data.info.avgRaiting} stars</h4>
-      <h4>{resObj.data.info.delTime} mins</h4>
-  </div>
+        <div className="res-card" >
+          <img className="res-logo" alt="BBQ" src={imageData} />
+          <h3>{name}</h3>
+          {cuisines.map((cuisine, i) => (
+            <h4 key={i}>{cuisine}</h4>
+          ))}
+          <h4>{avgRaiting} stars</h4>
+          <h4>{delTime} mins</h4>
+        </div>
   )
-  
 }   
 
-const resObj = {
-  data: {
+const resObj = [
+  {
+    data: {
       "info": {
         "id": "626148",
         "name": "La Pino'z Pizza",
@@ -69,18 +71,100 @@ const resObj = {
           "Pizzas",
         ],
         "avgRaiting":"4.2",
-        "delTime":"34"
+        "delTime":"34",
+        "imageData":'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ru7j7x8qqtvuzfuw7cdh'
       }
     }
-}
+  },
+  {
+    data: {
+      "info": {
+        "id": "123456",
+        "name": "Subway",
+        "cloudinaryImageId": "abc123",
+        "locality": "Downtown",
+        "areaName": "Central Square",
+        "costForTwo": "₹500 for two",
+        "cuisines": [
+          "Burgers",
+          "Fast Food"
+        ],
+        "avgRaiting":"4.5",
+        "delTime":"25",
+        "imageData":'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/63178e3e64d503a479f2a2048a474552'
+      }
+    }
+  },
+  {
+    data: {
+      "info": {
+        "id": "789012",
+        "name": "Sushi World",
+        "cloudinaryImageId": "xyz789",
+        "locality": "Midtown",
+        "areaName": "Broadway",
+        "costForTwo": "₹800 for two",
+        "cuisines": [
+          "Sushi",
+          "Japanese"
+        ],
+        "avgRaiting":"4.8",
+        "delTime":"40",
+        "imageData":'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/57ad35c7a51bfc3df3703fef028ad071'
+      }
+    }
+  },
+  {
+    data: {
+      "info": {
+        "id": "345678",
+        "name": "Taco Shack",
+        "cloudinaryImageId": "def456",
+        "locality": "Uptown",
+        "areaName": "Main Street",
+        "costForTwo": "₹300 for two",
+        "cuisines": [
+          "Tacos",
+          "Mexican"
+        ],
+        "avgRaiting":"4.3",
+        "delTime":"20",
+        "imageData":'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/7bb82538502ddf558058e8264d82d4c9'
+      }
+    }
+  },
+  {
+    data: {
+      "info": {
+        "id": "901234",
+        "name": "Spice Palace",
+        "cloudinaryImageId": "mno901",
+        "locality": "Chinatown",
+        "areaName": "Mott Street",
+        "costForTwo": "₹600 for two",
+        "cuisines": [
+          "Indian",
+          "Spicy Food"
+        ],
+        "avgRaiting":"4.6",
+        "delTime":"35",
+        "imageData":'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/puq9zrt9axb9hrrvljyr'
+      }
+    }
+  }
+];
+
 
 const Body = () =>{
   return (
     <div className="body">
       <div className="search">Search</div>
       <div className="res-container">
-        <RestaurantCard resObj={resObj}/>
-        {/* <RestaurantCard resName="KFC" cuisine="Fast Food,Wings,Burgers"/> */}
+        {resObj?.map((restaurant)=>{
+          return(
+            <RestaurantCard resObj={restaurant.data} key={restaurant.data.info.id}/>
+          )
+        })}
       </div>
     </div>
   )
--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
    # Props --> Props stands for properties basically just like in function we pass arguments similarly props are arguments which we pass into the component.
 
-   # Config Driven UI --> Lets suppose the case of swiggy website they show offers and coupons based on your locations and every locations will have some differenet offers so the developer will not make different set of offers for each location ,they will be controlling the UI using data or using configs which will be different for different locations as provided by backend API's. 
+   # Config Driven UI --> Lets suppose the case of swiggy website they show offers and coupons based on your locations and every locations will have some differenet offers so the developer will not make different set of offers for each location ,they will be controlling the UI using data or using configs which will be different for different locations as provided by backend API's.
+
+   # Why we need to use the key in map function --> Supoose we have not given unique id to the div which we are rendering on the DOM inside map function and we want to add a new div at end to the map so react will not know where to add it it will re render all the existing div and then this new added div is also rendered,but when you give the key it will treat every div as a unique element and will only rendered the div which is updating.
