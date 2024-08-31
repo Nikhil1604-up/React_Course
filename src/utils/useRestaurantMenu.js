67cacf7627e712c5f93dd2ad75feebfb92670f37
Nikhil <nikhilupadhyay1604@gmail.com>
2024-08-31 16:32:15 +0530

    wip:custom hooks implementation

---
+++ src/utils/useRestaurantMenu.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import { MENU_API } from "./constants";
+
+const useRestaurantMenu = (resId) => {
+  const [resInfo, setResInfo] = useState(null);
+  const [isLoading, setIsLoading] = useState(true); // Added loading state
+
+  const fetchMenu = async () => {
+    try {
+      const response = await fetch(`${MENU_API}${resId}`);
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const json = await response.json();
+      setResInfo(json.data);
+    } catch (error) {
+      console.error("Error fetching menu:", error);
+    } finally {
+      setIsLoading(false); // Set loading to false after fetching
+    }
+  };
+
+  useEffect(() => {
+    fetchMenu();
+  });
+
+  return { resInfo, isLoading };
+};
+
+export default useRestaurantMenu;
