a8ad50bab6e65a70fcc3c9c0ce216fdfc7e9d7c1
Nikhil <nikhilupadhyay1604@gmail.com>
2024-09-03 23:19:28 +0530

    add: higher order component implementation

--- src/Components/RestaurantCard.js
+++ src/Components/RestaurantCard.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const RestaurantCard = (props) => {
+  console.log(props);
   const { resObj } = props;
   const navigate = useNavigate();
   const { cloudinaryImageId, name, cuisines, avgRating, sla, id } = resObj.info;
@@ -26,6 +27,33 @@ const RestaurantCard = (props) => {
       <h4>{sla.deliveryTime} mins</h4>
     </div>
   );
+};
+
+// Higher Order Component
+
+// input - RestaurantCard ==> RestaurantCardPromoted Function/Component will be output.
+
+// Note --> A componet is a function which returns some piece of JSX.,therfore we have two return kind type of statements below
+
+export const withPromotedLabel = (RestaurantCard) => {
+  return (props) => {
+    return (
+      <div>
+        <label
+          style={{
+            position: "absolute",
+            margin: "20px",
+            padding: "3px",
+            color: "black",
+            backgroundColor: "red",
+          }}
+        >
+          Promoted
+        </label>
+        <RestaurantCard {...props} />
+      </div>
+    );
+  };
 };
 
 export default RestaurantCard;
